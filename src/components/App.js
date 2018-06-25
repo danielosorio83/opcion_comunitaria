fe2d1c6e608b843286f1bfecb0310d3e0e87fcff
Daniel Osorio <danielosorio83@gmail.com>
2018-06-25 16:51:27 -0500

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,19 @@
 import React, { Component, Fragment } from 'react';
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <header className="masthead mb-auto">
-          <div className="inner">
-            <a className="navbar-brand px-3 bg-warning" href="/">
-            </a>
-            <nav className="nav nav-masthead justify-content-center">
-              <a className="nav-link active" href='/'>Inicio</a>
-              <a className="nav-link" href='compaz.%PUBLIC_URL%'>Compaz</a>
-            </nav>
-          </div>
-        </header>
+        <main className="inner cover" role="main">
+          <img alt="Corporación Opción Comunitaria" src="./images/logo.jpg" className="img-fluid mb-5" />
+          <h1 className="cover-heading text-warning">Bienvenidos</h1>
+          <p className="lead mb-5">Queremos difundir, promover y gestionar la multiplicación de procesos comunitarios exitosos y sustentables que busquen mejorar la convivencia y la restitución y defensa de los derechos fundamentales consagrados en la Constitución Política de Colombia para sembrar raices para una paz estable y duradera.</p>
+          <p className="lead">
+            <a class="btn btn-lg btn-secondary" href="http://compaz.opcioncomunitaria.org/managers/sign_in">Ir a ComPAZ</a>
+            <a class="btn btn-lg btn-secondary" href="http://compaz-staging.opcioncomunitaria.org/managers/sign_in">Zona de Pruebas</a>
+          </p>
+        </main>
         <footer className="mastfoot mt-auto">
           <div className="inner">
             <p>© {(new Date()).getFullYear()} Corporación Opción Comunitaria</p>
