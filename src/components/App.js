7edf6e6d435c807fc2c35c9b17a2912e30b6968f
Daniel Osorio <danielosorio83@gmail.com>
2018-07-07 10:31:19 -0500

    test

--- src/components/App.js
+++ src/components/App.js
@@ -10,8 +10,11 @@ class App extends Component {
           <h1 className="cover-heading text-warning">Bienvenidos</h1>
           <p className="lead mb-5">Queremos difundir, promover y gestionar la multiplicación de procesos comunitarios exitosos y sustentables que busquen mejorar la convivencia y la restitución y defensa de los derechos fundamentales consagrados en la Constitución Política de Colombia para sembrar raices para una paz estable y duradera.</p>
           <p className="lead">
-            <a class="btn btn-lg btn-secondary" href="http://compaz.opcioncomunitaria.org/managers/sign_in">Ir a ComPAZ</a>
-            <a class="btn btn-lg btn-secondary" href="http://compaz-staging.opcioncomunitaria.org/managers/sign_in">Zona de Pruebas</a>
+            <a className="btn btn-lg btn-secondary mr-1" href="http://compaz.opcioncomunitaria.org/managers/sign_in">Ir a ComPAZ</a>
+            <a className="btn btn-lg btn-secondary" href="http://compaz-staging.opcioncomunitaria.org/managers/sign_in">Zona de Pruebas</a>
+          </p>
+          <p className="lead">
+            <a className="btn btn-lg btn-secondary" href="/centro_apoyo">Centro de Apoyo</a>
           </p>
         </main>
         <footer className="mastfoot mt-auto">
